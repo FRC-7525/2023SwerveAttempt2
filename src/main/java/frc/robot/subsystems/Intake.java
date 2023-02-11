3739e676ba2e82d0f140c1c44384c3734d6c7f53
Liquid2112 <ShubhPatel2504@gmail.com>
2023-02-10 18:11:11 -0600

    beam break implementation

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import frc.robot.Robot;
@@ -19,12 +20,13 @@ public class Intake {
     CANSparkMax rightWheel = new CANSparkMax(10, MotorType.kBrushless);
     Solenoid claw = new Solenoid(PneumaticsModuleType.REVPH, 1);
 
+    DigitalInput hasNoGamePiece = new DigitalInput(0);
+
     Robot robot;
     
     States state = States.OFF;
 
     boolean isCone = false;
-    boolean beamInterrupted = false;
 
     public Intake(Robot robot) {
         rightWheel.follow(leftWheel, true);
@@ -48,7 +50,7 @@ public class Intake {
             claw.set(isCone);
             leftWheel.stopMotor();
             // changes to hold once beam brake is interrupted
-            if (beamInterrupted) {
+            if (!hasNoGamePiece.get()) {
                 state = States.HOLD;
             }
             checkForOuttake();
@@ -61,7 +63,7 @@ public class Intake {
             // outtakes any game piece being held
             leftWheel.set(-1);
             // once the piece isn't sensed the claw is turned "off" 
-            if (!beamInterrupted) {
+            if (hasNoGamePiece.get()) {
                 state = States.OFF;
             }
         }
