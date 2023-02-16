dfa64601328e648c1a2f23a3b81042513e409daa
Liquid2112 <ShubhPatel2504@gmail.com>
2023-02-15 18:31:05 -0600

    Grammar

--- src/main/java/frc/robot/subsystems/RGB.java
+++ src/main/java/frc/robot/subsystems/RGB.java
@@ -15,12 +15,11 @@ enum RGBStates {
 }
 
 public class RGB {
-    Spark RGBcontroll = new Spark(0);
+    Spark RGBcontrol = new Spark(0);
     Robot robot = null;
 
     public RGB(Robot robot) {
         this.robot = robot;
-        
     }
 
     RGBStates state = RGBStates.Neutral;
@@ -32,11 +31,11 @@ public class RGB {
 
     public void periodic() {
         if (state == RGBStates.Cone) {
-            RGBcontroll.set(0.69);
+            RGBcontrol.set(0.69);
         } else if (state == RGBStates.Cube) {
-            RGBcontroll.set(0.89);
+            RGBcontrol.set(0.89);
         } else if (state == RGBStates.Neutral) {
-            RGBcontroll.set(0.99);
+            RGBcontrol.set(0.99);
         }
     }
 }
