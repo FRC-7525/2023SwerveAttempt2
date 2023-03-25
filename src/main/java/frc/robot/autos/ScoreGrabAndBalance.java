b48fc2501cb79032ae19e7043d41517944cee75f
Liquid2112 <ShubhPatel2504@gmail.com>
2023-03-25 14:11:32 -0500

    New approach to spin turn

--- src/main/java/frc/robot/autos/ScoreGrabAndBalance.java
+++ src/main/java/frc/robot/autos/ScoreGrabAndBalance.java
@@ -6,13 +6,14 @@ import frc.robot.subsystems.Swerve;
 import frc.robot.commands.AutoBalance;
 import frc.robot.commands.HoldCube;
 import frc.robot.commands.IntakeFromFloor;
+import frc.robot.commands.SpinTurnLuciaVersion;
 
 public class ScoreGrabAndBalance extends SequentialCommandGroup {    
     public ScoreGrabAndBalance(Robot robot, Swerve swerve) {
         addCommands(
-            new ScoreLevelThreeAuto(robot),
+           // new ScoreLevelThreeAuto(robot),
             new StraightMove(swerve, -6.0, true),
-            new SpinTurn(swerve, 180, false),
+            new SpinTurnLuciaVersion(swerve, 180),
            // new IntakeFromFloor(robot, swerve, 1, true, 0.8),
             new StraightMove(swerve, -2.3, true),
             new SideMove(swerve, 2.7, false),
